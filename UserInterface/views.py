d1ce0d96e897db415525de90008e8c9f21f70685
DefinitelyNotAnAssassin <dharklike@gmail.com>
2023-10-23 10:22:03 +0800

    Added Profiles

--- UserInterface/views.py
+++ UserInterface/views.py
@@ -2,21 +2,17 @@ from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from Models.models import * 
 from UserInterface.forms import ArticleForm
 from markdownx.utils import markdownify
+
+
 def index(request):
 
     print(Account.objects.all().order_by('role'))
     return render(request, 'UserInterface/index.html')
 
-
-
 def articles(request):
 
-
     q = Announcements.objects.all().order_by('-date')
-
     context = {
-
-
         'announcements' : q
     }
     return render(request, 'UserInterface/articles.html', context = context)
@@ -24,25 +20,21 @@ def articles(request):
 def view_article(request, id):
     article = get_object_or_404(Announcements, id = id)
     article.content = markdownify(article.content)
-    
+
     return render(request, 'UserInterface/view_article.html', context = {'article': article} )
 
 
 def about_us(request):
+
     officers = Account.objects.filter(role__range = [1, 11])
-    
     OrgRole = [(1, 'President'),(2, 'Internal Vice President'), (3, 'External Vice President'),
     (4, 'Treasurer'), (5, 'Secretary'), 
     (6, "Assistant Secretary"), (7, "Auditor"), (8, "Outreach Program Director"), (9, "Event Coordinator"),
     (10, "Public Information Officer"), (11, "Digital Officer"), (12, "Representative"), (13, 'Member')
-    
-    
     ]
-
-
     abc = dict(OrgRole)
     print(abc.get(5))
-    
+
     items = {
         'members': officers, 
         'role' : abc,  
@@ -51,7 +43,10 @@ def about_us(request):
 
 
 def contact_us(request):
-    return render(request, 'UserInterface/contact_us.html')
+    if request.method == 'POST':
+        return HttpResponse("Message sent")
+    elif request.method == 'GET':
+        return render(request, 'UserInterface/contact_us.html')
 
 
 def add_article(request):
