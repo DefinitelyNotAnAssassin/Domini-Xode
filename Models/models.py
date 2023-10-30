ec7d9370014dd79691fdc5c194d5b299a08772f3
DefinitelyNotAnAssassin <dharklike@gmail.com>
2023-10-30 16:53:44 +0800

    ReadtheDocs

--- Models/models.py
+++ Models/models.py
@@ -3,57 +3,108 @@ from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from markdownx.models import MarkdownxField
 
-# Create your models here.
-
-
-
-
-
 class Account(AbstractUser):
+    """
+    Custom user model representing an account in your organization.
+
+    Attributes:
+        role (int): The role of the user in the organization.
+        image_link (str): Link to the user's profile image.
+        description (str): A short description about the user.
+        facebook_link (str): Link to the user's Facebook profile.
+
+    Methods:
+        save(): Overrides the save method to set the user's password securely.
+        create_superuser(): Creates a superuser with specific attributes.
+
+    Meta:
+        ordering: Order users by their 'role' attribute.
+
+    __str__(): Returns a string representation of the user.
+
+    Inherits from AbstractUser.
+    """
     OrgRole = [(1, 'President'),(2, 'Internal Vice President'), (3, 'External Vice President'),
-    (4, 'Treasurer'), (5, 'Secretary'), 
-    (6, "Assistant Secretary"), (7, "Auditor"), (8, "Outreach Program Director"), (9, "Event Coordinator"),
-    (10, "Public Information Officer"), (11, "Digital Officer"), (12, "Representative"), (13, 'Member')
-    
-    
-    ]
-    role = models.IntegerField(choices=OrgRole, null = True, blank=True)
-    image_link = models.CharField(max_length=555, default = "https://preview.redd.it/h5gnz1ji36o61.png?width=225&format=png&auto=webp&s=84379f8d3bbe593a2e863c438cd03e84c8a474fa")
+               (4, 'Treasurer'), (5, 'Secretary'), 
+               (6, "Assistant Secretary"), (7, "Auditor"), (8, "Outreach Program Director"), (9, "Event Coordinator"),
+               (10, "Public Information Officer"), (11, "Digital Officer"), (12, "Representative"), (13, 'Member')
+              ]
+    role = models.IntegerField(choices=OrgRole, null=True, blank=True)
+    image_link = models.CharField(max_length=555, default="https://preview.redd.it/h5gnz1ji36o61.png?width=225&format=png&auto=webp&s=84379f8d3bbe593a2e863c438cd03e84c8a474fa")
     description = models.CharField(max_length=255, default="Description")
-    facebook_link = models.CharField(max_length=555, default = "https://www.facebook.com")
-    
+    facebook_link = models.CharField(max_length=555, default="https://www.facebook.com")
 
     def save(self, *args, **kwargs):
+        """
+        Overrides the save method to set the user's password securely.
+
+        Args:
+            *args: Additional arguments.
+            **kwargs: Additional keyword arguments.
+        """
         if not self.pk: 
             self.set_password(self.password)
         super().save(*args, **kwargs)
 
     def create_superuser(self, username, email, password=None, **extra_fields):
+        """
+        Creates a superuser with specific attributes.
+
+        Args:
+            username (str): The username for the superuser.
+            email (str): The email address for the superuser.
+            password (str): The password for the superuser.
+            **extra_fields: Additional fields for the superuser.
+        """
         user = self.create_user(username, email, password=password, is_staff=True, **extra_fields)
         user.is_active = True
         user.save(using=self._db)
         return
+
     class Meta:
         ordering = ['role']
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
-
 class Announcements(models.Model):
+    """
+    Model representing announcements or articles.
+
+    Attributes:
+        title (str): The title of the announcement or article.
+        author (Account): The author of the announcement, linked to the Account model.
+        content (MarkdownxField): The content of the announcement in Markdown format.
+        date (datetime): The date when the announcement was created.
+        thumbnail (ImageField): An optional thumbnail image for the announcement.
+
+    Methods:
+        __str__(): Returns a string representation of the announcement.
+    """
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Account, on_delete=models.CASCADE)
     content = MarkdownxField(max_length=10000)
-    date = models.DateTimeField(default = timezone.now)
-    thumbnail = models.ImageField(null = True, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+    thumbnail = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} | {self.author}"
-    
-
 
 
 class Messages(models.Model):
+    """
+    Model representing contact messages.
+
+    Attributes:
+        email (str): The email address of the sender.
+        full_name (str): The sender's full name.
+        phone_number (str): The sender's phone number.
+        msg (str): The message content.
+
+    Methods:
+        None
+    """
     email = models.CharField(max_length=64)
     full_name = models.CharField(max_length=128)
     phone_number = models.CharField(max_length=16)
--- UserInterface/views.py
+++ UserInterface/views.py
@@ -8,81 +8,134 @@ from django.contrib.auth import login as login_user
 from django.contrib.auth import authenticate
 
 def index(request):
+    """
+    Renders the index page.
+
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: Rendered index page.
+    """
     return render(request, 'UserInterface/index.html')
 
 def articles(request):
+    """
+    Renders a page displaying a list of articles.
 
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: Rendered articles page.
+    """
     q = Announcements.objects.all().order_by('-date')
     context = {
-        'announcements' : q
+        'announcements': q
     }
-    return render(request, 'UserInterface/articles.html', context = context)
+    return render(request, 'UserInterface/articles.html', context=context)
 
 def view_article(request, id):
-    article = get_object_or_404(Announcements, id = id)
+    """
+    Renders a page to view a specific article.
+
+    Args:
+        request (HttpRequest): The request object.
+        id (int): The ID of the article to view.
+
+    Returns:
+        HttpResponse: Rendered article view page.
+    """
+    article = get_object_or_404(Announcements, id=id)
     article.content = markdownify(article.content)
 
-    return render(request, 'UserInterface/view_article.html', context = {'article': article} )
-
+    return render(request, 'UserInterface/view_article.html', context={'article': article})
 
 def about_us(request):
+    """
+    Renders a page displaying information about the organization's officers and roles.
 
-    officers = Account.objects.filter(role__range = [1, 12])
-    OrgRole = [(1, 'President'),(2, 'Internal Vice President'), (3, 'External Vice President'),
-    (4, 'Treasurer'), (5, 'Secretary'), 
-    (6, "Assistant Secretary"), (7, "Auditor"), (8, "Outreach Program Director"), (9, "Event Coordinator"),
-    (10, "Public Information Officer"), (11, "Digital Officer"), (12, "Representative"), (13, 'Member')
-    ]
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: Rendered "about us" page.
+    """
+    officers = Account.objects.filter(role__range=[1, 12])
+    OrgRole = [(1, 'President'), (2, 'Internal Vice President'), (3, 'External Vice President'),
+               (4, 'Treasurer'), (5, 'Secretary'), (6, "Assistant Secretary"), (7, "Auditor"),
+               (8, "Outreach Program Director"), (9, "Event Coordinator"), (10, "Public Information Officer"),
+               (11, "Digital Officer"), (12, "Representative"), (13, 'Member')]
     abc = dict(OrgRole)
     print(abc.get(5))
 
     items = {
-        'members': officers, 
-        'role' : abc,  
+        'members': officers,
+        'role': abc,
     }
-    return render(request, 'UserInterface/about_us.html', context = items)
-
+    return render(request, 'UserInterface/about_us.html', context=items)
 
 def contact_us(request):
+    """
+    Renders a page for contacting the organization.
+
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: Rendered "contact us" page or handles form submission.
+    """
     if request.method == 'POST':
-        data = request.POST 
-        msg = Messages(email = data['email'], full_name = data['fullname'], msg = data['message'], phone_number = data['phone'])
+        data = request.POST
+        msg = Messages(email=data['email'], full_name=data['fullname'], msg=data['message'], phone_number=data['phone'])
         msg.save()
         messages.success(request, "Message sent. The organization will respond in the email / phone number provided.")
         return redirect("index")
     elif request.method == 'GET':
         return render(request, 'UserInterface/contact_us.html')
 
-
-
-
-@login_required 
+@login_required
 def add_article(request):
+    """
+    Renders a page for adding a new article or handles article submission.
 
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: Rendered "add article" page or redirects after submission.
+    """
     if request.method == 'GET':
         form = ArticleForm()
         items = {
             'form': form,
-
         }
-        return render(request, 'UserInterface/add_article.html', context = items)
+        return render(request, 'UserInterface/add_article.html', context=items)
 
     elif request.method == 'POST':
         payload = request.POST
-        new_article = Announcements(title = payload['title'], author = request.user, content = payload['content'])
+        new_article = Announcements(title=payload['title'], author=request.user, content=payload['content'])
         new_article.save()
         return redirect('articles')
 
-
 def login(request):
+    """
+    Renders a login page or handles user authentication.
+
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: Rendered login page or a success message upon successful login.
+    """
     if request.method == 'GET':
         return render(request, 'UserInterface/login.html')
     elif request.method == 'POST':
-        isExisting = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+        isExisting = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if isExisting:
             login_user(request, isExisting)
-            return HttpResponse("Login Succesfully")
-        else: 
+            return HttpResponse("Login Successfully")
+        else:
             redirect('login')
         return HttpResponse("To implement")
     else:
