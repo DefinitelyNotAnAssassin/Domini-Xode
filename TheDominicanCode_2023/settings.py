eea65d4a749c4fe928c14c2ab3e3c65209a6392e
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-08-20 20:47:48 +0800

    chore: Update SECRET_KEY generation in settings.py

--- TheDominicanCode_2023/settings.py
+++ TheDominicanCode_2023/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import secrets
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets.token_hex(32)
+SECRET_KEY = env('SECURITY_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -34,6 +37,7 @@ ALLOWED_HOSTS = ['tdc.pythonanywhere.com',
 # Application definition
 
 INSTALLED_APPS = [
+    'unfold',
     'API.apps.ApiConfig',
     'markdownx',
     'django.forms',
