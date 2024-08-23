4714fd3779066ad51ad3e04b52a0aeb33320204f
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-08-24 02:34:09 +0800

    Add new URLs for mission and testimonials pages

--- About/urls.py
+++ About/urls.py
@@ -4,4 +4,6 @@ from About import views
 urlpatterns = [
     path('about_us', views.about_us, name = 'about us'),
     path('yearlist', views.yearlist, name = 'yearlist'),
+    path('mission', views.mission, name = 'mission'),
+    path('testimonials', views.testimonials, name = 'testimonials'),
 ]
--- About/views.py
+++ About/views.py
@@ -30,3 +30,33 @@ def yearlist(request):
         return render(request, 'About/yearlist.html', context = items)
 
 
+
+def mission(request):
+    """
+    Renders a page displaying the organization's mission statement.
+
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: Rendered "mission" page.
+    """
+    if get_referer(request):
+        return render(request, 'About/partition/mission.html')
+    else:
+        return render(request, 'About/mission.html')
+
+def testimonials(request):
+    """
+    Renders a page displaying testimonials from organization members.
+
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: Rendered "testimonials" page.
+    """
+    if get_referer(request):
+        return render(request, 'About/partition/testimonials.html')
+    else:
+        return render(request, 'About/testimonials.html')
