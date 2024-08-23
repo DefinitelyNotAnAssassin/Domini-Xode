da7ff22876b4d5c5f9d2d8cea682aceb428fe85a
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-08-24 01:20:43 +0800

    REFACTOR TO TAILWIND CSS

---
+++ Events/templatetags/custom_filter.py
@@ -0,0 +1,30 @@
+from django.template.defaulttags import register
+from django.utils import timezone
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+
+@register.filter 
+def date_status(date):
+    if timezone.now() < date.event_start_date:
+        return "Upcoming"
+    elif timezone.now() < date.event_start_date and timezone.now() > date.event_end_date:
+        return "Ongoing"
+
+
+    else:
+        return "Done"
+    
+
+@register.filter 
+def event_status(date):
+    if timezone.now() < date.event_start_date:
+        return "secondary"
+    elif timezone.now() < date.event_start_date and timezone.now() > date.event_end_date:
+        return "success"
+
+
+    else:
+        return "dark"
+    
